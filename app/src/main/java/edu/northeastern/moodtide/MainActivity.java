9ef014857a479bbc6e72aa482dea4ba38fe482fa
zzeng01 <clessej119@gmail.com>
2024-03-24 20:59:28 -0400

    Implemented basic logic of bottom navigation menu

--- app/src/main/java/edu/northeastern/moodtide/MainActivity.java
+++ app/src/main/java/edu/northeastern/moodtide/MainActivity.java
@@ -2,13 +2,27 @@ package edu.northeastern.moodtide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView addEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addEntry = (ImageView) findViewById(R.id.nav_home);
+        addEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
