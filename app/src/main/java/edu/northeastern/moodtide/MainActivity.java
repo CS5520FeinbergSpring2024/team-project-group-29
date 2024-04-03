c850bd1dc87ed18685f2a65394ffec34e9c027eb
SI201 <software_intro@163.com>
2024-04-02 22:54:01 -0400

    implement select trigger and add customized trigger

--- app/src/main/java/edu/northeastern/moodtide/MainActivity.java
+++ app/src/main/java/edu/northeastern/moodtide/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import edu.northeastern.moodtide.addEntry.SelectTrigger;
 import edu.northeastern.moodtide.addEntry.SelectionActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,16 +22,20 @@ public class MainActivity extends AppCompatActivity {
 
         addEntry = (ImageView) findViewById(R.id.nav_home);
         addEntry.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, SelectionActivity.class);
-//                startActivity(intent);
-//            }
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectionActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        // just for test purpose
+        Button selectTriggerButton = findViewById(R.id.button_select_trigger);
+        selectTriggerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent_select_trigger = new Intent(MainActivity.this, SelectionActivity.class);
-                startActivity(intent_select_trigger);
+                Intent selectTriggerIntent = new Intent(MainActivity.this, SelectTrigger.class);
+                startActivity(selectTriggerIntent);
             }
         });
     }
--- app/src/main/java/edu/northeastern/moodtide/addEntry/SelectTrigger.java
+++ app/src/main/java/edu/northeastern/moodtide/addEntry/SelectTrigger.java
@@ -1,11 +1,22 @@
 package edu.northeastern.moodtide.addEntry;
 
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.ContextThemeWrapper;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.flexbox.FlexboxLayout;
@@ -17,56 +28,194 @@ import edu.northeastern.moodtide.R;
 import edu.northeastern.moodtide.object.Trigger;
 
 public class SelectTrigger extends AppCompatActivity {
-    List<Trigger> triggerList;
+    ObservableTriggerList triggerList;
     FlexboxLayout flexboxLayout;
+    String currentMood;
+
+    ArrayList<Trigger> selectedTriggers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_trigger);
 
+        //set the title and color according to the selected mood
+        TextView textViewTitle = findViewById(R.id.text_trigger_tile);
+        textViewTitle.setText("What makes you" + currentMood + "?");
+        //todo set the tile color
+
+
         // find the flexbox layout
         flexboxLayout = findViewById(R.id.flexlayout_triggers);
+
+        // add your own trigger
+        addYourOwnTrigger();
+
+        // initiate predefined triggers
         initiateDefinedTriggers();
 
-        // click button to add a trigger
+
+
+
+
 
 
     }
 
     public void initiateDefinedTriggers() {
         // predefined triggers
-        triggerList = new ArrayList<>();
-        triggerList.add(new Trigger("Food"));
-        triggerList.add(new Trigger("Weather"));
-        triggerList.add(new Trigger("Friend"));
-        triggerList.add(new Trigger("Family"));
-        triggerList.add(new Trigger("Work"));
-        triggerList.add(new Trigger("School"));
+        triggerList = new ObservableTriggerList();
+
+        // set listener when a trigger added, generate a textview add to flexbox
+        triggerList.setOnItemAddedListener(new ObservableTriggerList.OnItemAddedListener() {
+            @Override
+            public void onItemAdded(Trigger trigger) {
+                // Create and add a TextView for the new trigger
+                TextView textView = new TextView(new ContextThemeWrapper(SelectTrigger.this, R.style.TextViewWithBorder), null, 0);
+                textView.setText(trigger.getName());
+
+                // click to select multiple triggers
+                selectedTriggers = new ArrayList<>();
+                final boolean[] isHighlighted = {false};
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int index = flexboxLayout.indexOfChild(view);
+                        GradientDrawable background = (GradientDrawable) textView.getBackground();
+                        if(index != flexboxLayout.getChildCount() - 1) {
+                            if(!isHighlighted[0]) {
+                                // change
+                                isHighlighted[0] = true;
+                                // add selected trigger to the list
+                                selectedTriggers.add(triggerList.getTriggerAt(index));
+                                Log.d("SELECTED TRIGGER", triggerList.getTriggerAt(index).getName());
+                                // change the color of the border
+
+                                background.setStroke(3, getResources().getColor(R.color.selected_trigger_highlighted_border)); // New border color
+                            } else {
+                                selectedTriggers.remove(triggerList.getTriggerAt(index));
+                                Log.d("REMOVED TRIGGER", triggerList.getTriggerAt(index).getName());
+                                background.setStroke(3, Color.BLACK); // New border color
+
+                            }
 
 
-        // generate flexbox layout
-        for (int i = 0; i <triggerList.size(); i++) {
-            Trigger trigger = triggerList.get(i);
-            TextView textView = new TextView(new ContextThemeWrapper(this, R.style.TextViewWithBorder), null, 0);
-            textView.setText(trigger.getName());
+                        }
 
-            // Set layout params for the TextView
-            FlexboxLayout.LayoutParams layoutParams = new FlexboxLayout.LayoutParams(
-                    FlexboxLayout.LayoutParams.WRAP_CONTENT,
-                    FlexboxLayout.LayoutParams.WRAP_CONTENT
-            );
-            layoutParams.setMargins(10, 10, 10, 10); // Optional: set margins
-            textView.setLayoutParams(layoutParams);
-            flexboxLayout.addView(textView);
-        }
+
+                    }
+                });
+
+                FlexboxLayout.LayoutParams layoutParams = new FlexboxLayout.LayoutParams(
+                        FlexboxLayout.LayoutParams.WRAP_CONTENT,
+                        FlexboxLayout.LayoutParams.WRAP_CONTENT
+                );
+                layoutParams.setMargins(10, 10, 10, 10); // Optional: set margins
+                textView.setLayoutParams(layoutParams);
+
+
+                // always add to the last but one
+                flexboxLayout.addView(textView, flexboxLayout.getChildCount() - 1);
+
+
+            }
+        });
+
+        // add predefined triggers to the list
+        triggerList.addTrigger(new Trigger("Food"));
+        triggerList.addTrigger(new Trigger("Weather"));
+        triggerList.addTrigger(new Trigger("Friend"));
+        triggerList.addTrigger(new Trigger("Family"));
+        triggerList.addTrigger(new Trigger("Work"));
+        triggerList.addTrigger(new Trigger("School"));
+
+
+
 
     }
 
-//    public void addTrigger(View view) {
-//        int id = view.getId();
-//        if(id == R.id.button_add_trigger) {
-//            triggerList.add(new Trigger(""))
-//        }
-//    }
+    public void addYourOwnTrigger() {
+        // append the addTriggerView at the end
+        EditText addTriggerView = new EditText(new ContextThemeWrapper(this, R.style.TextViewWithBorder), null, 0);
+        addTriggerView.setText(" + ");
+        FlexboxLayout.LayoutParams layoutParams = new FlexboxLayout.LayoutParams(
+                FlexboxLayout.LayoutParams.WRAP_CONTENT,
+                FlexboxLayout.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(10, 10, 10, 10); // Optional: set margins
+        addTriggerView.setLayoutParams(layoutParams);
+        flexboxLayout.addView(addTriggerView);
+        addTriggerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // set the tect to empty
+                addTriggerView.setText("");
+                // Make sure the EditText can gain focus
+                addTriggerView.setFocusableInTouchMode(true);
+                addTriggerView.requestFocus();
+
+                // Open the keyboard for text input
+                addTriggerView.clearFocus();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(addTriggerView, InputMethodManager.SHOW_IMPLICIT);
+
+
+            }
+        });
+
+        // when the edit action is done (press th return )
+        addTriggerView.setInputType(InputType.TYPE_CLASS_TEXT);
+        addTriggerView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        addTriggerView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                // Check if the action is a "Done" action
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    String newTrigger = addTriggerView.getText().toString();
+                    if(!newTrigger.equals("")) {
+                        // add trigger
+                        triggerList.addTrigger(new Trigger(newTrigger));
+                    }
+                    addTriggerView.setText(" + ");
+                    Log.e("edit done", "edit done");
+                    // hide the keyboard
+                    InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    // Return true to indicate you have handled the event
+                    return true;
+                }
+                // Return false to let the keyboard handle the event (e.g., hiding itself)
+                return false;
+            }
+        });
+
+    }
+
+    // observer design pattern detecting the new trigger added and generate new textView to flexbox layout
+    private static class ObservableTriggerList {
+        private ArrayList<Trigger> list = new ArrayList<>();
+        private OnItemAddedListener listener;
+
+        public interface OnItemAddedListener {
+            void onItemAdded(Trigger trigger);
+        }
+
+        public void setOnItemAddedListener(OnItemAddedListener listener) {
+            this.listener = listener;
+        }
+
+        public void addTrigger(Trigger trigger) {
+            list.add(trigger);
+            if (listener != null) {
+                listener.onItemAdded(trigger);
+            }
+        }
+
+        public Trigger getTriggerAt(int index) {
+            return list.get(index);
+        }
+
+        // Other necessary list methods can be added here (e.g., get, remove)
+    }
 }
+
