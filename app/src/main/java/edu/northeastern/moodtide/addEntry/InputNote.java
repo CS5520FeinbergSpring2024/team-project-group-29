0b7c4f231c827c92e463eb7b4e6c4b764f163a92
SI201 <software_intro@163.com>
2024-04-03 17:30:54 -0400

    add note page

---
+++ app/src/main/java/edu/northeastern/moodtide/addEntry/InputNote.java
@@ -0,0 +1,16 @@
+package edu.northeastern.moodtide.addEntry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.moodtide.R;
+
+public class InputNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_note);
+    }
+}
