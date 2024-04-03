f5b691913a560e83963a98a4762fe4c2d1e503a9
SI201 <software_intro@163.com>
2024-04-03 16:51:29 -0400

    main page welcome quote from API

--- app/src/main/java/edu/northeastern/moodtide/MainActivity.java
+++ app/src/main/java/edu/northeastern/moodtide/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 
 import edu.northeastern.moodtide.addEntry.SelectTrigger;
 import edu.northeastern.moodtide.addEntry.SelectionActivity;
+import edu.northeastern.moodtide.getQuote.GetQuote;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // display daily quote
+        Thread thread = new Thread(new GetQuote(this));
+        thread.start();
+        // add entry when clicking "+"
         addEntry = (ImageView) findViewById(R.id.nav_home);
         addEntry.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/edu/northeastern/moodtide/getQuote/NetworkUtils.java
@@ -0,0 +1,39 @@
+package edu.northeastern.moodtide.getQuote;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setRequestProperty("X-RapidAPI-Key", "97f883113fmsh2bb288b45a62afap1b4269jsn6bf9531b3c53");
+        urlConnection.setRequestProperty("X-RapidAPI-Host", "positivity-tips.p.rapidapi.com");
+        urlConnection.setDoInput(true);
+        urlConnection.connect();
+
+
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            // convert this stream into a String, which represents the full response content.
+            Scanner scanner = new Scanner(in);
+            // sets the delimiter to the beginning of the input, effectively telling the Scanner to tokenize the entire stream as a single token.
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
