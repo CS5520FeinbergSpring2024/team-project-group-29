44534f1c8127a4a48470fb56e8b7a8de411de7b2
SI201 <software_intro@163.com>
2024-04-03 20:34:03 -0400

    setup database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,5 +38,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.flexbox:flexbox:3.0.0") // Replace with the latest version
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0")) // firebase BoM
+    implementation("com.google.firebase:firebase-database")
 
 }
