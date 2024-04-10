fda8b358c4d134aea159326b96d2754d29022cc6
zzeng01 <clessej119@gmail.com>
2024-04-10 17:28:40 -0400

    Implemented calendar view icon corresponding to date with entries

--- app/src/main/java/edu/northeastern/moodtide/calendarView/CalendarActivity.java
+++ app/src/main/java/edu/northeastern/moodtide/calendarView/CalendarActivity.java
@@ -1,56 +1,60 @@
 package edu.northeastern.moodtide.calendarView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
+
 import androidx.fragment.app.FragmentManager;
-import android.widget.TextView;
 
 import com.applandeo.materialcalendarview.CalendarDay;
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.listeners.OnCalendarDayClickListener;
-import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+import com.applandeo.materialcalendarview.listeners.OnCalendarPageChangeListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 import edu.northeastern.moodtide.R;
 import edu.northeastern.moodtide.object.Entry;
 import edu.northeastern.moodtide.object.User;
+import edu.northeastern.moodtide.shapes.CustomDotDrawable;
 
 public class CalendarActivity extends AppCompatActivity {
 
     private CalendarView calendarView;
     DatabaseReference userRef, dateRef;
+    HashMap<String, HashMap<String, Entry>> record;
+    Set<String> validDates;
     User user;
+    List<String> daysWithEntry;
+    List<CalendarDay> calendarDays;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        calendarView = findViewById(R.id.calendarView);
         String uid = getSharedPreferences("memory", Context.MODE_PRIVATE).getString("uid", "");
         userRef = FirebaseDatabase.getInstance().getReference(uid);
         getUser();
-
-        calendarView = findViewById(R.id.calendarView);
 
         calendarView.setOnCalendarDayClickListener(new OnCalendarDayClickListener() {
             @Override
@@ -66,6 +70,18 @@ public class CalendarActivity extends AppCompatActivity {
                 }
             }
         });
+        calendarView.setOnPreviousPageChangeListener(new OnCalendarPageChangeListener() {
+            @Override
+            public void onChange() {
+                paintCurrentMonth();
+            }
+        });
+        calendarView.setOnForwardPageChangeListener(new OnCalendarPageChangeListener() {
+            @Override
+            public void onChange() {
+                paintCurrentMonth();
+            }
+        });
     }
 
     public void getUser() {
@@ -76,18 +92,93 @@ public class CalendarActivity extends AppCompatActivity {
                 if (dataSnapshot.exists()) {
                     // User data exists at the specified path
                     user = dataSnapshot.getValue(User.class);
+                    record = user.getData();
+                    validDates = record.keySet();
+                    paintCurrentMonth();
                 }
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {}
         });
     }
 
+    public void paintCurrentMonth() {
+        Calendar currentMonthCalendar = calendarView.getCurrentPageDate();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM", Locale.getDefault());
+        String currentMonthYearString = dateFormat.format(currentMonthCalendar.getTime());
+        Log.e("Get month",currentMonthYearString);
+        calendarDays = new ArrayList<>();
+        for(String date:validDates){
+            String entryMonthYear = date.substring(0, 7);
+            if(currentMonthYearString.equals(entryMonthYear)){
+                Log.e("Get month",date);
+                paintDate(date);
+            }
+        }
+        Log.e("painted?",Integer.toString(calendarDays.size()));
+        calendarView.setCalendarDays(calendarDays);
+        //calendarView.clearSelectedDays();
+
+    }
+    public void paintDate(String date){
+        List<Entry> entries = getEntries(date);
+        Set<Integer> colors = new HashSet<>();
+        for(Entry e: entries){
+            String category=e.getCategory();
+            Log.e("Get entry",category);
+            int colorID=-1;
+            switch (category){
+                case "Love":
+                    colorID=R.color.yellow_theme;
+                    break;
+                case "Anger":
+                    colorID=R.color.pink_theme;
+                    break;
+                case "Joy":
+                    colorID=R.color.green_theme;
+                    break;
+                case "Surprise":
+                    colorID=R.color.aqua_theme;
+                    break;
+                case "Sadness":
+                    colorID=R.color.ocean_theme;
+                    break;
+                case "Fear":
+                    colorID=R.color.orange_theme;
+            }
+            colors.add(colorID);
+        }
+        int[] colorList = new int[colors.size()];
+        int i=0;
+        for(int color:colors){
+            colorList[i++]=color;
+        }
+        CustomDotDrawable dots = new CustomDotDrawable(this,colorList);
+        if(dots!=null){Log.e("paint","paint");}
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+
+            // Parse the date string into a Date object
+            Date dateFormatted = dateFormat.parse(date);
+
+            // Convert the Date object into a CalendarDay object
+            if (date != null) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(dateFormatted);
+                CalendarDay calendarDay= new CalendarDay(calendar);
+                calendarDay.setImageDrawable(dots);
+                calendarDays.add(calendarDay);
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace(); // Handle parsing exception if necessary
+        }
+
+    }
+
 
     private List<Entry> getEntries(String date){
 
-        HashMap<String, HashMap<String, Entry>> record = user.getData();
         if(!record.containsKey(date)){return null;}
         else{
             List<Entry> entries=new ArrayList<>(record.get(date).values());
---
+++ app/src/main/java/edu/northeastern/moodtide/shapes/CustomDotDrawable.java
@@ -0,0 +1,93 @@
+package edu.northeastern.moodtide.shapes;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+public class CustomDotDrawable extends Drawable {
+
+    private static final int MAX_DOTS = 6;
+    private static final int ROWS = 2;
+    private static final int COLS = 3;
+    private static final int DOT_SIZE_PERCENTAGE = 30; // Percentage of canvas width for dot size
+
+    private int numDots;
+    private int[] dotColors;
+
+    public CustomDotDrawable(Context context, int[] dotColors) {
+        this.numDots=dotColors.length;
+        this.dotColors = new int[numDots];
+        for (int i = 0; i < numDots; i++) {
+            this.dotColors[i] = ContextCompat.getColor(context, dotColors[i]);
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        int canvasWidth = canvas.getWidth();
+        int canvasHeight = canvas.getHeight();
+        Log.e("width",Integer.toString(canvasWidth));
+        Log.e("height",Integer.toString(canvasHeight));
+
+        // Calculate dot size based on canvas width
+        float dotSize = canvasWidth * DOT_SIZE_PERCENTAGE / 100;
+        Log.e("canvas",Float.toString(dotSize));
+
+        // Calculate spacing between dots
+        float horizontalSpacing = (canvasWidth - COLS * dotSize) / (COLS + 1);
+        float verticalSpacing = (canvasHeight - ROWS * dotSize) / (ROWS + 1);
+
+        // Create paint for drawing dots
+        Paint dotPaint = new Paint();
+        dotPaint.setStyle(Paint.Style.FILL);
+
+        // Draw dots
+        for (int i = 0; i < MAX_DOTS; i++) {
+            int row = i / COLS;
+            int col = i % COLS;
+
+            Log.e("row",Integer.toString(row));
+            Log.e("column",Integer.toString(col));
+
+
+            float x = (col + 1) * horizontalSpacing + col * dotSize;
+            float y = (row + 1) * verticalSpacing + row * dotSize;
+            Log.e("x",Float.toString(x));
+            Log.e("y",Float.toString(y));
+
+            if (i < numDots) {
+                dotPaint.setColor(dotColors[i]);
+                Log.e("color",Integer.toString(dotColors[i]));
+                canvas.drawCircle(x + dotSize / 2, y + dotSize / 2, dotSize / 2, dotPaint);
+            } else {
+                // Draw transparent dot (or white dot) to hide excess dots
+                dotPaint.setColor(Color.TRANSPARENT); // Transparent color
+                canvas.drawCircle(x + dotSize / 2, y + dotSize / 2, dotSize / 2, dotPaint);
+            }
+        }
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        // This method is required to implement due to Drawable class, but we can leave it empty
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        // This method is required to implement due to Drawable class, but we can return a default value
+        return PixelFormat.OPAQUE;
+    }
+}
